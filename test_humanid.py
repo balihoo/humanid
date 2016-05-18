adbce6e61b6de7ff20a54561b1c54d1974d25276
gerry ens <gens@balihoo.com>
2016-05-18 16:42:22 -0600

    master: fix tests after wordlist reorder...

--- test_humanid.py
+++ test_humanid.py
@@ -60,7 +60,7 @@ class TestHumanid(unittest.TestCase):
         hexin = 'e5dc0c113b1541b4b97a029be34904aa'
         lists = (self.hid.nouns, self.hid.adjectives, self.hid.ofstuff)
         (noun, adj, stuff) = self.hid._words(hexin, lists)
-        self.assertEqual((noun, adj, stuff), ('norse', 'instructive', 'primality'))
+        self.assertEqual((noun, adj, stuff), ('applewood', 'worried', 'primality'))
         # now test that not providing a hash does not match
         self.assertNotEqual(
             self.hid._words(None, lists),
@@ -76,7 +76,7 @@ class TestHumanid(unittest.TestCase):
 
     def test_rpg_item(self):
         hexin = 'e5dc0c113b1541b4b97a029be34904aa'
-        expected = "foregoing##ballpark##of##primality"
+        expected = "experienced##grasshopper##of##primality"
         rpg = self.hid.rpg_item(separator='##', hexstr=hexin)
         self.assertEqual(rpg, expected)
         (uuid, rpg) = self.hid.rpg_item(separator='##', hexstr=hexin, return_hash=True)
@@ -87,7 +87,7 @@ class TestHumanid(unittest.TestCase):
 
     def test_band_name(self):
         hexin = 'e5dc0c113b1541b4b97a029be34904aa'
-        expected = "norse and the instructive wounds"
+        expected = "applewood and the worried clams"
         band = self.hid.band_name(separator=' ', hexstr=hexin)
         self.assertEqual(band, expected)
         (uuid, band) = self.hid.band_name(separator=' ', hexstr=hexin, return_hash=True)
